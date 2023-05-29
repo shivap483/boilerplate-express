cac469e7ea8cf5ddeeec7330575ba5c0da4fbb22
shiva <shivap483@gmail.com>
2023-05-29 18:31:43 +0530

    myApp.js

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let app = express();
-port = 3000
-app.listen(port, () => {
-  console.log(`Node is listening on port ${port}...`);
-})
+// port = 3000
+// app.listen(port, () => {
+//   console.log(`Node is listening on port ${port}...`);
+// })
 app.get("/", (req, res) => {
     const message = 'Hello Express'
     console.log(`${message}`)
