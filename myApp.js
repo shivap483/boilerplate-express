3ea9d4a002e9b79efe8f01882b8f410bba5ac525
shiva <shivap483@gmail.com>
2023-05-30 09:54:42 +0530

    added .env file

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
@@ -11,6 +12,11 @@ app.get("/", (req, res) => {
 
 app.get("/json", (req, res) => {
     const obj = {message: "Hello json"};
+    const upperCase = process.env.MESSAGE_STYLE
+    if(upperCase == "uppercase") {
+        const message = obj.message
+        obj.message = message.toUpperCase();
+    }
     res.json(obj)
 })
 
