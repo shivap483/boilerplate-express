9548a6d5f8f201b2b91490c8943e29eab9b86a26
shiva <shivap483@gmail.com>
2023-05-30 10:11:59 +0530

    added root level request logger

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ let app = express();
 
 const absolutePath = __dirname + '/public'
 app.use('/public', express.static(absolutePath))
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+})
 
 app.get("/", (req, res) => {
     const absolutePath = __dirname + '/views/index.html'
